36176c4376762f5d542fecace746688f50dd35b0
paulchristianlabor <paulchristianlaborofficial@gmail.com>
2022-12-03 21:30:04 +0800

    update html, css and javascript

--- main.js
+++ main.js
@@ -19,3 +19,38 @@ function onClick() {
         closeNav()
     }
 }
+
+const carousel = document.querySelector(".carousel"),
+arrowIcons = document.querySelectorAll(".wrapper i");
+
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        console.log(icon);
+    });
+});
+
+const dragStart = (e) => {
+    // Updating global variables value on mouse down event
+    isDragStart = true;
+    prevPageX = e.pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    // Scrolling images/carousel to left according to mouse pointer
+    if(!isDragStart) return;
+    e.preventDefault();
+    let positionDiff = e.pageX - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+}
+
+const dragStop = () => {
+    isDragStart = false;
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
